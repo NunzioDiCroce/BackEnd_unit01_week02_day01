f8b128d0be9a705ae6c08369e9c114b74edc8045
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-03 15:28:24 +0200

    Commit, esercizio1 work in progress

---
+++ src/Esercizio1_Package/Esercizio1_Main.java
@@ -0,0 +1,67 @@
+package Esercizio1_Package;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Esercizio1_Main {
+
+	public static void main(String[] args) {
+
+		// array definition
+		int[] myArray = new int[5];
+
+		// random items definition
+		int item1 = new Random().nextInt(10) + 1;
+		int item2 = new Random().nextInt(10) + 1;
+		int item3 = new Random().nextInt(10) + 1;
+		int item4 = new Random().nextInt(10) + 1;
+		int item5 = new Random().nextInt(10) + 1;
+
+		// array population
+		myArray[0] = item1;
+		myArray[1] = item2;
+		myArray[2] = item3;
+		myArray[3] = item4;
+		myArray[4] = item5;
+
+		// array print
+		System.out.println("");
+		System.out.println("ESERCIZIO 1");
+		System.out.println("- - - - - - - - - - - - - - - - - - - -");
+		System.out.println("");
+		System.out.println(
+				"Array: " + myArray[0] + " " + myArray[1] + " " + myArray[2] + " " + myArray[3] + " " + myArray[4]);
+
+		// array editing
+		Scanner esercizioUno = new Scanner(System.in);
+
+		String position;
+		int positionInt = 1;
+		String newItem;
+		int newItemInt = 1;
+
+		while (newItemInt != 0) {
+			System.out.println("");
+			System.out.println("Scegli la posizione dell'array da 0 a 4 che vuoi modificare:");
+			position = esercizioUno.nextLine();
+			positionInt = Integer.parseInt(position);
+			System.out.println("");
+			System.out.println("Indica il nuovo intero da 1 a 10 che vuoi inserire nella posizione scelta:");
+			newItem = esercizioUno.nextLine();
+			newItemInt = Integer.parseInt(newItem);
+
+			myArray[positionInt] = newItemInt;
+
+			System.out.println("");
+			System.out.println("Nuovo Array: " + myArray[0] + " " + myArray[1] + " " + myArray[2] + " " + myArray[3]
+					+ " " + myArray[4]);
+		}
+
+		myArray[positionInt] = newItemInt;
+
+		System.out.println("");
+		System.out.println("Termine programma");
+
+	}
+
+}
