f1f0a1b832af04e9c47064ec3e235e4c68ea235d
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-03 16:50:16 +0200

    Commit, esercizio3 work in progress

--- src/Esercizio3_Package/BancaException.java
+++ src/Esercizio3_Package/BancaException.java
@@ -2,6 +2,20 @@ package Esercizio3_Package;
 
 public class BancaException extends Exception {
 
+	// attributes
+	public String message;
+
 	// constructor
+	public BancaException(String _message) {
+
+		super(_message);
+		this.message = _message;
+	}
+
+	@Override
+	public String toString() {
+		return message;
+
+	}
 
 }
